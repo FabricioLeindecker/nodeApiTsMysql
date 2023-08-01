ab4d2a52950938567ad6a885fba6ea247d1d2ff6
Fabricio <fabricio_lopes@outlook.com.br>
2023-08-01 19:51:50 -0300

    chore: init server

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+const express = require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 3333;
+
+app.get('/', (request: any, response: any) => {
+    response.send('Bem-vindo!')
+});
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`)
+});
